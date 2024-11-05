697aeb73fbf16649a89a90af872abd4372ac5d11
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-11-05 23:45:23 +0100

    Added contract public fields and constructor

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,5 +2,27 @@
 pragma solidity 0.8.26;
 
 contract ManualToken {
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
+
+    /**
+     * Constructor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    constructor(
+        uint256 initialSupply,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) {
+        totalSupply = initialSupply * 10 ** uint256(decimals); // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply; // Give the creator all initial tokens
+        name = tokenName; // Set the name for display purposes
+        symbol = tokenSymbol; // Set the symbol for display purposes
+    }
   
 }
